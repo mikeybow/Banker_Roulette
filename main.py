689a8c3aa3c4a36c34535fd2ca3c92b62a43a9be
mikeybow <micbowman25@gmail.com>
2021-11-30 11:00:43 -0600

    finished

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+names_string = input("Give me everybody's names, seperated by a comma. ")
+names = names_string.split(", ")
+
+num_items = len(names)
+
+random_choice = random.randint(0, num_items - 1)
+person_who_pays = names[random_choice]
+print(person_who_pays + " is going to buy the meal today.")
